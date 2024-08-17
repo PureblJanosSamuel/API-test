424d26d7bf825cbee36a08b351d4fcded29f5235
PureblJanosSamuel <purebl.janos.samuel@gmail.com>
2024-08-17 15:16:55 +0200

    Simplifing Endpoints

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 SWAPI_BASE_URL = "https://swapi.dev/api/"
 
+def fech_data(id, endpoint):
+  response = requests.get(f"{SWAPI_BASE_URL}{endpoint}/{id}")
+  if response.status_code == 200:
+    return jsonify(response.json())
+  else:
+    return jsonify({"error":"Person cannot found!"}), 404
+
 @app.route("/")
 def home():
   return jsonify({"message":"You are at Home!"})
 
 @app.route('/people/<int:id>', methods=['GET'])
 def get_person(id):
-  response = requests.get(f"{SWAPI_BASE_URL}people/{id}/")
-  if response.status_code == 200:
-    return jsonify(response.json())
-  else:
-    return jsonify({"error":"Person cannot found!"}), 404
+  return fech_data(id,"people")
 
 if __name__ == "__main__":
   app.run(debug=True)
